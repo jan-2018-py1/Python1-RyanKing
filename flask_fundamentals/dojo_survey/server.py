a24279812991042a05fadfeba9da521ed3b0852d
Ryan King <ryanmking62@gmail.com>
2018-01-22 18:24:07 -0800

    finished dojo survey validation

--- flask_fundamentals/dojo_survey/server.py
+++ flask_fundamentals/dojo_survey/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash
 app = Flask(__name__)
+app.secret_key = "aaaaaa"
 
 @app.route('/')
 def index():
@@ -7,13 +8,16 @@ def index():
 
 @app.route("/process", methods=["POST"])
 def process():
-    if len(request.form["name"] < 1:
+    if len(request.form["name"]) < 1:
         flash("Name cannot be empty!")
         return redirect('/')
-    if len(request.form["comments"] < 1:
+    if len(request.form["comments"]) < 1:
         flash("Comments cannot be empty!")
+        return redirect('/')
+    elif len(request.form["comments"]) > 120:
+        flash("Comments too long!")
+        return redirect('/')
 
-    
     name = request.form["name"]
     location = request.form["location"]
     language = request.form["language"]
