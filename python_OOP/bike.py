e4e0dc673e37b6a21ef90cdead0c3e53a367f9e8
Ryan King <ryanmking62@gmail.com>
2018-02-06 02:05:39 -0800

    added bike project

---
+++ python_OOP/bike.py
@@ -0,0 +1,29 @@
+class Bike(object):
+    def __init__(self, price, max_speed):
+        self.price = price
+        self.max_speed = max_speed
+        self.miles = 0
+
+    def displayInfo(self):
+        print "Price:", self.price
+        print "Max Speed:", self.max_speed
+        print "Total Miles:", self.miles
+        return self
+
+    def ride(self):
+        print "Riding"
+        self.miles += 10
+        return self
+
+    def reverse(self):
+        print "Reversing"
+        self.miles -= 5
+        return self
+
+bike1 = Bike(200, "25mph")
+bike2 = Bike(250, "32mph")
+bike3 = Bike(180, "28mph")
+
+bike1.ride().ride().ride().reverse().displayInfo()
+bike2.ride().ride().reverse().reverse().displayInfo()
+bike3.reverse().reverse().reverse().displayInfo()
