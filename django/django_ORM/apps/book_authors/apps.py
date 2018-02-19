a6af74116aa199ed73e137fdece7ade79555b427
Ryan King <ryanmking62@gmail.com>
2018-02-19 15:10:28 -0800

    fixed dojo ninja file structure

--- django/django_ORM/apps/book_authors/apps.py
+++ django/django_ORM/apps/book_authors/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class MainConfig(AppConfig):
-    name = 'main'
+class BookAuthorsConfig(AppConfig):
+    name = 'book_authors'
--- django/django_ORM/django_ORM/wsgi.py
+++ django/django_ORM/django_ORM/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dojo_ninjas project.
+WSGI config for django_ORM project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dojo_ninjas.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_ORM.settings")
 
 application = get_wsgi_application()
